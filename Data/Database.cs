2c7f527e8fae56b243c200f68543c1081a1c55df
Håkan Edling <hakan@tidyui.com>
2012-09-14 13:43:15 +0200

    Update script for extension entities.

--- Data/Database.cs
+++ Data/Database.cs
@@ -21,7 +21,7 @@ namespace Piranha.Data
 		/// <summary>
 		/// Gets the current database version.
 		/// </summary>
-		public static int CurrentVersion = 13 ;
+		public static int CurrentVersion = 14 ;
 
 		/// <summary>
 		/// Gets the currently logged in users identity.
