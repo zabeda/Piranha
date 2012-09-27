434c0c153023651e0ef531cfb2547dd69ac8cefa
Håkan Edling <hakan@tidyui.com>
2012-09-27 14:45:35 +0200

    Updated the MenuLink hook.

--- Delegates.cs
+++ Delegates.cs
@@ -20,7 +20,7 @@ namespace Piranha
 		public delegate void HeadHook(Web.UIHelper ui, StringBuilder str, Models.Page page, Models.Post post) ;
 
 		public delegate void MenuItemHook(Web.UIHelper ui, StringBuilder str, Models.Sitemap page, bool active, bool activechild) ;
-		public delegate void MenuItemLinkHook(Web.UIHelper ui, StringBuilder str, string title, string url) ;
+		public delegate void MenuItemLinkHook(Web.UIHelper ui, StringBuilder str, Models.Sitemap page) ;
 		public delegate void MenuLevelHook(Web.UIHelper ui, StringBuilder str, string cssclass) ;
 
 		public delegate void ModelLoadedHook<T>(Controller controller, Manager.MenuItem menu, T model) ;
