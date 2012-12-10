566713ebec3a1ed5f45c2c6274630d4168ba7c9b
Håkan Edling <hakan@tidyui.com>
2012-12-10 09:52:46 +0100

    Updated YUI-compressor package.

---
+++ Piranha/Extend/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Piranha.Extend
+{
+	/// <summary>
+	/// Base class for defining a Piranha module.
+	/// </summary>
+	public abstract class Module
+	{
+		/// <summary>
+		/// Ensures the module.
+		/// </summary>
+		public virtual void Ensure() {
+		}
+	}
+}
