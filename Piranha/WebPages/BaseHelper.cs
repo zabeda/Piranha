fca05372ffb8bdae53683c95f850da8b8359eb8f
Håkan Edling <hakan@tidyui.com>
2012-12-11 11:45:46 +0100

    Added base class for Page helpers.

---
+++ Piranha/WebPages/BaseHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Piranha.WebPages
+{
+	public class BaseHelper : System.Web.WebPages.HelperPage
+	{
+		#region Members
+		private static Web.SiteHelper siteHelper = null ;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Gets the helper for the piranha methods.
+		/// </summary>
+		public static PiranhaHelper UI {
+			get {
+				return new PiranhaHelper(CurrentPage, Html) ;
+			}
+		}
+
+		/// <summary>
+		/// Gets the helper for the piranha site.
+		/// </summary>
+		public static Web.SiteHelper Site { 
+			get {
+				if (siteHelper == null)
+					siteHelper = new Web.SiteHelper() ;
+				return siteHelper ;
+			}
+		}
+		#endregion
+	}
+}
