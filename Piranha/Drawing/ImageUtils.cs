f8bf4ead67aaa4206007d024fa1f79f20f160809
Håkan Edling <hakan@tidyui.com>
2012-12-21 11:07:45 +0100

    Fixed bug that triggered the resize method when requesting the original image.

--- Piranha/Drawing/ImageUtils.cs
+++ Piranha/Drawing/ImageUtils.cs
@@ -56,20 +56,22 @@ namespace Piranha.Drawing
 		/// <returns>The resized image</returns>
 		public static Image Resize(Image img, int width, int height) {
 			// We only reduce size and crop, we don't magnify images
-			if (width <= img.Width && height <= img.Height) {
-				var xRatio = width / (double)img.Width ;
-				var yRatio = height / (double)img.Height ;
+			if (!(img.Width == width && img.Height == height)) {
+				if (width <= img.Width && height <= img.Height) {
+					var xRatio = width / (double)img.Width ;
+					var yRatio = height / (double)img.Height ;
 
-				if (img.Height * xRatio < height)
-					img = Resize(img, Convert.ToInt32(img.Width * yRatio)) ;
-				else img = Resize(img, Convert.ToInt32(width)) ;
+					if (img.Height * xRatio < height)
+						img = Resize(img, Convert.ToInt32(img.Width * yRatio)) ;
+					else img = Resize(img, Convert.ToInt32(width)) ;
 
-				var newRect = new Rectangle(((width - img.Width) / 2) * -1, ((height - img.Height) / 2) * -1, width, height) ;
-				var orgBmp = new Bitmap(img) ;
-				var crpBmp = orgBmp.Clone(newRect, img.PixelFormat) ;
-				orgBmp.Dispose() ;
+					var newRect = new Rectangle(((width - img.Width) / 2) * -1, ((height - img.Height) / 2) * -1, width, height) ;
+					var orgBmp = new Bitmap(img) ;
+					var crpBmp = orgBmp.Clone(newRect, img.PixelFormat) ;
+					orgBmp.Dispose() ;
 
-				return (Image)crpBmp ;
+					return (Image)crpBmp ;
+				}
 			}
 			return img ;
 		}
