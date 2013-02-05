096783711bc6b4dfb3101566568748ec741767d5
Håkan Edling <hakan@tidyui.com>
2013-02-06 00:00:55 +0100

    Added login-screen

---
+++ Piranha.Manager/Framework/Models/AccountLoginModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+
+using Piranha.Web;
+
+namespace Piranha.Manager.Models
+{
+	/// <summary>
+	/// View model for the login page.
+	/// </summary>B
+	public class AccountLoginModel
+	{
+		#region Properties
+		/// <summary>
+		/// Gets/sets the current version.
+		/// </summary>
+		[ModelProperty(OnLoad="LoadVersion")]
+		public string Version { get ; set ; }
+		#endregion
+
+		/// <summary>
+		/// Loads the current version.
+		/// </summary>
+		public void LoadVersion() {
+			Version = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion ;
+		}
+	}
+}
