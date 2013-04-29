d96e49576c2066a2a0b49b82c59751c5734bb042
Håkan Edling <hakan@tidyui.com>
2013-04-29 07:11:54 +0200

    Added fixed toolbars.

--- Piranha/Areas/Manager/Content/Js/jquery.form.js
+++ Piranha/Areas/Manager/Content/Js/jquery.form.js
@@ -5,9 +5,11 @@
  */
 
 var posFixed = null;
+var posTools = null;
 
 $(document).ready(function () {
     posFixed = $('.btn-content').position();
+    posTools = $('td.tools').position();
 
     $('.main-content .tools a').click(function () {
         var id = $(this).attr('data-id');
@@ -22,6 +24,14 @@ $(document).ready(function () {
 });
 
 $(window).bind('scroll', function (e) {
+    if (posTools) {
+        if ($(this).scrollTop() + 55 > posTools.top) {
+            $('td.tools').addClass('fixed-toolbar');
+        } else {
+            $('td.tools').removeClass('fixed-toolbar');
+        }
+    }
+
     if ($(this).scrollTop() > posFixed.top + 20) {
         if (!isFixed)
             $('.mce-toolbar').hide();
