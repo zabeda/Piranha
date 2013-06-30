837b0277b7d64339c44a34d7857bfdbb13943a83
Håkan Edling <hakan@tidyui.com>
2013-06-30 23:49:58 +0200

    Added interface for the media cache provider.

---
+++ Piranha/IO/IMediaCacheProvider.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Piranha.IO
+{
+	/// <summary>
+	/// The media cache provider handles all cached resized images from both the
+	/// media library, as well as media uploaded from the application.
+	/// </summary>
+	public interface IMediaCacheProvider
+	{
+		/// <summary>
+		/// Gets the data for the cached image with the given dimensions. In case of
+		/// a cache miss null is returned.
+		/// </summary>
+		/// <param name="id">The id</param>
+		/// <param name="width">The width of the image</param>
+		/// <param name="height">The optional height of the image</param>
+		/// <param name="type">The media type</param>
+		/// <returns>The binary data, null in case of a cache miss</returns>
+		byte[] Get(Guid id, int width, int? height, MediaType type = MediaType.Media) ;
+
+		/// <summary>
+		/// Gets the draft data for the cached image with the given dimensions. In case of
+		/// a cache miss null is returned.
+		/// </summary>
+		/// <param name="id">The id</param>
+		/// <param name="width">The width of the image</param>
+		/// <param name="height">The optional height of the image</param>
+		/// <param name="type">The media type</param>
+		/// <returns>The binary data, null in case of a cache miss</returns>
+		byte[] GetDraft(Guid id, int width, int? height, MediaType type = MediaType.Media) ;
+
+		/// <summary>
+		/// Stores the given cache data for the image with the given id.
+		/// </summary>
+		/// <param name="id">The id</param>
+		/// <param name="data">The media data</param>
+		/// <param name="width">The width of the image</param>
+		/// <param name="height">The optional height of the image</param>
+		/// <param name="type">The media type</param>
+		void Put(Guid id, byte[] data, int width, int? height, MediaType type = MediaType.Media) ;
+
+		/// <summary>
+		/// Stores the given cache data for the image draft with the given id.
+		/// </summary>
+		/// <param name="id">The id</param>
+		/// <param name="data">The media data</param>
+		/// <param name="width">The width of the image</param>
+		/// <param name="height">The optional height of the image</param>
+		/// <param name="type">The media type</param>
+		void PutDraft(Guid id, byte[] data, int width, int? height, MediaType type = MediaType.Media) ;
+
+		/// <summary>
+		/// Deletes all cached images related to the given id, both draft and published.
+		/// </summary>
+		/// <param name="id">The id</param>
+		/// <param name="type">The media type</param>
+		void Delete(Guid id, MediaType type = MediaType.Media) ;
+	}
+}
