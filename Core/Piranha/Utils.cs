cd3b0f188a39a0a565673c7e245d244e96f8fec7
Håkan Edling <hakan@tidyui.com>
2014-03-17 13:19:06 +0100

    Added utility class to use within the core framework as well as in modules and applications.

---
+++ Core/Piranha/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Piranha
+{
+	/// <summary>
+	/// Helper class with utility methods
+	/// </summary>
+	public static class Utils
+	{
+		/// <summary>
+		/// Generates a slug of the given string
+		/// </summary>
+		/// <param name="str">The input string</param>
+		/// <returns>The sluggified string</returns>
+		public static string GenerateSlug(string str) { 
+			var slug = Regex.Replace(str.ToLower()
+				.Replace(" ", "-")
+				.Replace("å", "a")
+				.Replace("ä", "a")
+				.Replace("á", "a")
+				.Replace("à", "a")
+				.Replace("ö", "o")
+				.Replace("ó", "o")
+				.Replace("ò", "o")
+				.Replace("é", "e")
+				.Replace("è", "e")
+				.Replace("í", "i")
+				.Replace("ì", "i"), @"[^a-z0-9-/]", "").Replace("--", "-");
+
+			if (slug.EndsWith("-"))
+				slug = slug.Substring(0, slug.LastIndexOf("-"));
+			if (slug.StartsWith("-"))
+				slug = slug.Substring(Math.Min(slug.IndexOf("-") + 1, slug.Length));
+			return slug;
+		}
+	}
+}
