fe72e51164fb83fb487965c7bbdc9cad633f92d0
Håkan Edling <hakan@tidyui.com>
2015-05-05 11:32:38 +0200

    Updated service registration. Added NuSpec for services.

---
+++ Legacy/Piranha.Legacy.Services/ServiceType.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright (c) 2015 Håkan Edling
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ * 
+ * http://github.com/piranhacms/piranha
+ * 
+ */
+
+using System;
+
+namespace Piranha.Legacy.Services
+{
+	/// <summary>
+	/// The different services available.
+	/// </summary>
+	[Flags]
+	public enum ServiceType
+	{
+		All = 1,
+		Category = 2,
+		Changes = 4,
+		Media = 8,
+		Page = 16,
+		PageType = 32,
+		Post = 64,
+		PostType = 128,
+		SiteMap = 256
+	}
+}
