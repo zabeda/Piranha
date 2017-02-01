81f8b159012fc64c3a4aea8609ec0f0171d2a5d6
Filip Jansson <filip.jansson@outlook.com>
2017-02-01 11:42:01 +0100

    Added cropping options to content when resizing

--- Core/Piranha/Drawing/ImageUtils.cs
+++ Core/Piranha/Drawing/ImageUtils.cs
@@ -12,6 +12,7 @@ using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.IO;
+using Piranha.Models;
 
 namespace Piranha.Drawing
 {
@@ -61,19 +62,31 @@ namespace Piranha.Drawing
 		/// <param name="width">The desired width</param>
 		/// <param name="height">The desired height</param>
 		/// <returns>The resized image</returns>
-		public static Image Resize(Image img, int width, int height) {
+		public static Image Resize(Image img, int width, int height, HorizontalCropping cropH, VerticalCropping cropV) {
 			// We only reduce size and crop, we don't magnify images
 			if (!(img.Width == width && img.Height == height)) {
 				if (width <= img.Width && height <= img.Height) {
-					var xRatio = width / (double)img.Width;
+                    var xRecPos = 0;
+                    var yRecPos = 0;
+                    var xRatio = width / (double)img.Width;
 					var yRatio = height / (double)img.Height;
 
-					if (img.Height * xRatio < height)
-						img = Resize(img, Convert.ToInt32(img.Width * yRatio));
-					else img = Resize(img, Convert.ToInt32(width));
+                    if (img.Height * xRatio < height)
+                        img = Resize(img, Convert.ToInt32(img.Width * yRatio));
+                    else img = Resize(img, Convert.ToInt32(width));
 
-					var newRect = new Rectangle(((width - img.Width) / 2) * -1, ((height - img.Height) / 2) * -1, width, height);
-					var orgBmp = new Bitmap(img);
+                    if (cropH == HorizontalCropping.Center)
+                        xRecPos = (img.Width - width) / 2;
+                    else if (cropH == HorizontalCropping.Right)
+                        xRecPos = img.Width - width;
+
+                    if (cropV == VerticalCropping.Center)
+                        yRecPos = (img.Height - height) / 2;
+                    else if (cropV == VerticalCropping.Bottom)
+                        yRecPos = img.Height - height;
+
+                    var newRect = new Rectangle(xRecPos, yRecPos, width, height);
+                    var orgBmp = new Bitmap(img);
 					var crpBmp = orgBmp.Clone(newRect, img.PixelFormat);
 					orgBmp.Dispose();
 
